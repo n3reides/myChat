3dc234679fc96c42c904b6c698c03ea4f82039d7
n3reides <33963844+n3reides@users.noreply.github.com>
2017-11-25 18:06:46 +0100

    SKAPAR NYTT START WINDOW

--- MyChat.java
+++ MyChat.java
@@ -1,32 +1,28 @@
 package mychat;
-
 import java.io.*;
-import java.net.*;
-
-
 public class MyChat {
-    
-    //Olle Dahlstedt & Max Sonebäck
-    // Uppgift 3; 2017-11-24
 
+    //Olle Dahlstedt & Max Sonebäck
+    // Uppgift 4; 2017-11-24
     private static final int PORT = 8191; // 2^13 - 1, PRIME
 
     public static void main(String[] args) throws IOException {
-        boolean SERVER_STARTED = false;       
+        boolean SERVER_STARTED = true;
         if (!SERVER_STARTED) {
             Server myServer = new Server(PORT);
             SERVER_STARTED = !SERVER_STARTED;
         } else {
-            String localhost = InetAddress.getLocalHost().getHostAddress();
-            Client myClient = new Client(localhost, PORT); 
-       // NewContact newContact = new NewContact();
-       // newContact.setVisible(true);
+            StartWindow startWindow = new StartWindow();
         }
+
     }
+
+    /* String IP = lines.get(1);
+            int PORT = Integer.parseInt(lines.get(2));
+            Client myClient = new Client(IP, PORT); 
+         NewContact newcontact = new NewContact();
+         newcontact.setVisible(true);*/
 }
-
-
-
 
 
 
