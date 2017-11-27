c99cced737a99f501b7e7c7ae9d2d031d0453be7
n3reides <33963844+n3reides@users.noreply.github.com>
2017-11-27 20:01:37 +0100

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,27 +2,45 @@
 package mychat;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
 
+class Server implements ObjectStreamListener {
 
-class Server {
-    
-    ServerSocket serverSocket;
-    Socket mySocket;
-    ChatParticipant server;
+    ArrayList<ObjectOutputStream> outputStreamArray = new ArrayList<>();
+    ArrayList<ObjectStreamManager> streamManagerArray = new ArrayList<>();
+    ArrayList<Socket> socketArray = new ArrayList<>();
 
     Server(int port) throws IOException {
-        try {
-            String name = "SERVER";
-            serverSocket = new ServerSocket(port);
-            mySocket = serverSocket.accept();
-            server = new ChatParticipant(mySocket, name);
-        } catch (IOException e) {
-            System.out.println("IOException in Server, could not connect");
-        } finally {
-            serverSocket.close();
-        }
+        PingThread pingServer = new PingThread(port, this);
+        pingServer.start();
+        
     }
-    
+
+    @Override
+    public void objectReceived(int number, Object object, Exception exception) {
+        System.out.println("We are in server OR");
+        if (exception == null) {
+            System.out.println("we have an object");
+            String message = (String) object;
+            for (ObjectOutputStream OS : outputStreamArray) {
+                try {
+                    OS.writeObject(message);
+                    OS.flush();
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+
+        }
+
+    }
+
 }
