71a985ec90e9e39f0c15cb5be8972cb816fc0955
Max <Max@130.238.252.6>
2017-11-28 11:37:07 +0100

    Lade till print localAdress

---
+++ MyChat5/src/mychat/MyChat.java
@@ -0,0 +1,30 @@
+package mychat;
+
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MyChat {
+
+    //Olle Dahlstedt & Max Sonebäck
+    // Uppgift 4; 2017-11-24
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(InetAddress.getLocalHost().getHostAddress‌​());
+        MainWindow mainFrame = new MainWindow();
+        
+    }
+
+
+}
+
